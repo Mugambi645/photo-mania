3d1ec3dc727a9ea5b73570663dc4dbb226b1b020
mugambi645 <mugambi645@gmail.com>
2021-11-26 19:52:22 +0300

    added postgres driver and configured settings to communicate with postgres database

--- photomania/photomania/settings.py
+++ photomania/photomania/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'photomania.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'pato',
+    'PASSWORD':'flower2',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
