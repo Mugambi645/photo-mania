bcd9c08545b1505633915023bab7441e03743464
mugambi645 <mugambi645@gmail.com>
2021-11-27 07:51:12 +0300

    media root settings

--- photomania/gallery/urls.py
+++ photomania/gallery/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "gallery"
 urlpatterns = [
-
+    path('search/', views.search_results, name='search'),
+    path('location/', views.image_location, name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
